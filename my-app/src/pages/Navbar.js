50d2846011b0c0e84e2f608fcb021a32aa011144
Aurelisa Sindhunirmala <aurelisajuans@gmail.com>
2024-07-13 19:55:17 -0700

    updated generate plan page

--- my-app/src/pages/Navbar.js
+++ my-app/src/pages/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import logo from '../pics/logo.png';
 
 const Navbar = () => {
   return (
     <nav className="sideNav">
       <div className="up">
-        <a href="#"> About Us </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <a href="#" className="about"> About Us </a>
         <h2> Other Features</h2>
         <a href="#"> Generate Plan </a>
         <a href="#"> Generate Qiz </a>
