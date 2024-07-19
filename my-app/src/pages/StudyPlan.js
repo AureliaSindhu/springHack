f47d404855c5d7d8e6e422bf9872d28be00e1d67
Brittany Chan <brittanyhaileec@gmail.com>
2024-07-18 19:36:23 -0700

    css for study plan page

--- my-app/src/pages/StudyPlan.js
+++ my-app/src/pages/StudyPlan.js
@@ -1,31 +1,89 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Navbar from './Navbar';
 import {Link} from 'react-router-dom';
+import sideline from '../pics/side-line.png';
+import qLogo from '../pics/q-logo.png';
 
 function StudyPlan() {
     const [planDetails, setPlanDetails] = useState('Here you can add details about your study plan.');
+    const fileInputRef = useRef(null);
 
     useEffect(() => {
         // Example of side effects or data fetching
         console.log('StudyPlan component mounted');
     }, []);
 
+    const handleUploadClick = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
+    const [file, setFile] = useState(null);
+
+    
+
     return (
-        <div className="study-plan">
-            <Navbar />
-            <div className="header-plan">
-                <div className="header-left">
-                    <Link to="/"> Back </Link>
+        <div className="plan-container">
+  
+            <div className="left-side mt-4">
+                <Navbar />
+                
+            </div>
+            <img src={sideline} alt="line" className="side-line" />
+            
+
+            <div className="right-side col-9">
+                <div className="header-plan mt-4">
+                    <Link to="/" className="header-plan-left-back">
+                        <i className="bi bi-arrow-left my-1 p-2" alt="back" />Back
+                    </Link>
+                    <button className="header-right-btn2 mt-3" onClick={handleUploadClick}>
+                        Upload +
+                    </button>
+
                 </div>
-                <div className="header-right">
-                    {/* <button className="signIn" onClick={handleUploadClick}> Upload + </button> */}
+                <div className="plan-content">
+                    <h1 className="m-4">Generate <span className="plan-text"> Plan </span></h1>
+                    
+                    <div className="input-section">
+                        <p>{planDetails}</p>
+                       
+
+                        {/* Show file upload button */}
+                        <button className="input-button" onClick={handleUploadClick}>
+                            <div className="input-box">
+                                <h2> <span class="click-here">Click here </span>to upload your
+                                    schedule and we'll do the rest!
+                                </h2>
+                                {/* <button type='button' onClick={handleUploadClick}>
+                                    Upload File
+                                </button> */}
+                
+                                <img src={qLogo} alt="q Logo" />
+                            </div>
+                        </button>
+
+                        <input
+                            type='file'
+                            id='file'
+                            name='file'
+                            accept=".pdf,.docx,.pptx,.txt"
+                            ref={fileInputRef}
+                            // onChange={handleFileChange}
+                            style={{ display: 'none' }}
+                        />
+
+                      
+                    </div>
                 </div>
             </div>
 
-            <h1>Generate <span> Plan </span></h1>
-            <p>{planDetails}</p>
+            
+            
         </div>
+        
     );
 }
 
 export default StudyPlan;
+
